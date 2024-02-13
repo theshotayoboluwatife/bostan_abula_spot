ee5b78f194c24e88d2dde61969946451fc408daa
theshotayoboluwatife <shotayobolu@gmail.com>
2024-02-13 09:01:55 +0100

    started sign up form

---
+++ lib/features/authentication/presentation/widgets/signup_form.dart
@@ -0,0 +1,62 @@
+import 'package:AbulaBostan/features/authentication/presentation/widgets/text_fields.dart';
+import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
+
+import '../../../../widgets/custom_button.dart';
+import '../../../../widgets/custom_text.dart';
+
+class SignUpForm extends StatefulWidget {
+  const SignUpForm({Key? key}) : super(key: key);
+
+  @override
+  State<SignUpForm> createState() => _SignUpFormState();
+}
+
+class _SignUpFormState extends State<SignUpForm> {
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const Gap(30.0),
+          CustomText(
+            text: 'Email address',
+            fontSize: 15,
+            color: Colors.black.withOpacity(0.5),
+          ),
+          const Gap(10.0),
+          EmailTextField(
+            controller: emailController,
+            textInputType: TextInputType.emailAddress,
+          ),
+          const Gap(20.0),
+          CustomText(
+            text: 'Password',
+            fontSize: 15,
+            color: Colors.black.withOpacity(0.5),
+          ),
+          const Gap(10.0),
+          PasswordTextField(
+            controller: passwordController,
+            textInputType: TextInputType.visiblePassword,
+          ),
+          const Gap(20.0),
+          TextButton(
+            onPressed: () {},
+            child: const CustomText(
+              text: 'Forgot password?',
+              color: Color(0xffFA4A0C),
+            ),
+          ),
+          const Gap(50.0),
+          AppButton(onPressed: () {}, text: 'Login'),
+          const Gap(50.0),
+        ],
+      ),
+    );
+  }
+}
