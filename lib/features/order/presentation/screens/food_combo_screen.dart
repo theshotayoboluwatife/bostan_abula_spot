091a92f2040e1b7ae08c6f1651848999bbbaf841
Shotayo Boluwatife <shotayobolu@gmail.com>
2024-04-21 21:39:05 +0100

    food category tab collapses

--- lib/features/order/presentation/screens/food_combo_screen.dart
+++ lib/features/order/presentation/screens/food_combo_screen.dart
@@ -1,6 +1,4 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:gap/gap.dart';
 
 import '../../../../widgets/custom_text.dart';
@@ -14,16 +12,18 @@ class FoodComboScreen extends StatelessWidget {
       body: Container(
         padding: const EdgeInsets.all(20.0),
         color: const Color(0xffEDEDED),
-        child: const SafeArea(
+        child: SafeArea(
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              CustomText(
+              const CustomText(
                 text: 'Make your order',
                 fontSize: 20,
                 fontWeight: FontWeight.bold,
                 textAlign: TextAlign.center,
               ),
-              Gap(20),
+              const Gap(20),
               FoodCategoryWidget(
                 foodName: 'Swallow',
               ),
@@ -42,7 +42,6 @@ class FoodComboScreen extends StatelessWidget {
               FoodCategoryWidget(
                 foodName: 'Drinks',
               ),
-
             ],
           ),
         ),
@@ -53,18 +52,21 @@ class FoodComboScreen extends StatelessWidget {
 
 class FoodCategoryWidget extends StatelessWidget {
   final String? foodName;
+  bool isCollapsed;
 
-  const FoodCategoryWidget({
-    super.key,
-    required this.foodName,
-  });
+  void toggleArrowIcon() {
+    isCollapsed = !isCollapsed;
+  }
+
+  FoodCategoryWidget(
+      {super.key, required this.foodName, this.isCollapsed = false});
 
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
         Align(
-          alignment: Alignment.center,
+          alignment: Alignment.centerLeft,
           child: Row(
             mainAxisSize: MainAxisSize.min,
             mainAxisAlignment: MainAxisAlignment.start,
@@ -76,13 +78,15 @@ class FoodCategoryWidget extends StatelessWidget {
                 fontWeight: FontWeight.w500,
               ),
               IconButton(
-                icon: const Icon(Icons.keyboard_arrow_down_sharp),
-                onPressed: () {},
+                icon: isCollapsed
+                    ? const Icon(Icons.keyboard_arrow_down_sharp)
+                    : const Icon(Icons.keyboard_arrow_up_sharp),
+                onPressed: () => toggleArrowIcon,
               )
             ],
           ),
         ),
-        const Gap(16),
+        const Gap(8),
       ],
     );
   }
